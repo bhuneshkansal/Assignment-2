fa6ffe0a4055e217679d351f1398da0a5b7ac005
bhuneshkansal <104307528+bhuneshkansal@users.noreply.github.com>
2022-05-15 22:05:45 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,7 @@
+str="Python is a case sensitive language"
+print(len(str))
+print(str[::-1])
+print(str[10:35])
+print(str.replace("a case sensitive language", "object oriented"))
+print(str.find("a"))
+print(str.replace(" ",""))

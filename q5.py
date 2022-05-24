288aac3088d118689f42d75f98e11fe606ca99db
bhuneshkansal <104307528+bhuneshkansal@users.noreply.github.com>
2022-05-24 13:36:15 +0530

    Update q5.py

--- q5.py
+++ q5.py
@@ -1,13 +1,9 @@
+a = int(input('Enter the length of the 1st side: '))
+b = int(input('Enter the length of the 2nd side: '))
+c = int(input('Enter the length of the 3rd side: '))
+while (a+b>c) and (b+c>a) and (c+a>b):
+    print("Yes")
+    break
+else:
+    print("No")
 
-def is_triangle(a, b, c):
-    if  c>(a+b) or b>(a+c) or a>(b+c):
-        print('No')
-    else:
-        print('Yes')
-def triangle():
-    a = int(input('Enter the length of the 1st side: '))
-    b = int(input('Enter the length of the 2nd side: '))
-    c = int(input('Enter the length of the 3rd side: '))
-    is_triangle(a, b, c)
-    
-triangle()
